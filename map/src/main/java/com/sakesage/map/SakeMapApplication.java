0343fa7515df74b0b530fed8241f3418ef387e4d
Sunny Lee <86199470+sunnnycode@users.noreply.github.com>
2024-10-13 16:46:45 +0900

    initial commit

---
+++ map/src/main/java/com/sakesage/map/SakeMapApplication.java
@@ -0,0 +1,13 @@
+package com.sakesage.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakeMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakeMapApplication.class, args);
+	}
+
+}
---
+++ map/src/test/java/com/sakesage/map/SakeMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakesage.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakeMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
